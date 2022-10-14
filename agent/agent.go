4241b295da63071fa010b5caeaf0e52c4f8f551f
Jason Dew <jason.dew@gmail.com>
2022-10-14 13:12:01 -0400

    Successfully joined the channel

--- agent/agent.go
+++ agent/agent.go
@@ -7,13 +7,13 @@ import (
 	"os"
 	"strings"
 
-  "github.com/gorilla/websocket"
+  "golang.org/x/net/websocket"
 	"golang.org/x/net/html"
 )
 
 func Execute() {
-	requestURL := "http://localhost:4000"
-	request, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	pageUrl := "http://fanatics.localhost:4000/breaking/44a9547d-a1a6-4a6e-94a5-3c5c4de66993"
+	request, err := http.NewRequest(http.MethodGet, pageUrl, nil)
 	if err != nil {
 		fmt.Printf("could not create request: %s\n", err)
 		os.Exit(1)
@@ -24,15 +24,41 @@ func Execute() {
 		fmt.Printf("error making HTTP request: %s\n", err)
 		os.Exit(1)
 	}
+	defer response.Body.Close()
 
-	responseBody, err := ioutil.ReadAll(response.Body)
+	responseBodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Printf("error reading HTTP response: %s\n", err)
 		os.Exit(1)
 	}
 
-	csrfToken := parseCsrfToken(fmt.Sprintf("%s", responseBody))
-	fmt.Println(csrfToken)
+	responseBody := fmt.Sprintf("%s", responseBodyBytes)
+	csrfToken := parseCsrfToken(responseBody)
+	viewId, viewSession, viewStatic := parseLiveViewData(responseBody)
+
+	websocketUrl := fmt.Sprintf("ws://fanatics.localhost:4000/live/websocket?_csrf_token=%s&timezone=America/New_York&vsn=2.0.0", csrfToken)
+	wsConfig, err := websocket.NewConfig(websocketUrl, pageUrl)
+	wsConfig.Header.Set("Cookie", response.Header.Get("Set-Cookie"))
+	ws, err := websocket.DialConfig(wsConfig)
+
+	if err != nil {
+		fmt.Printf("error making WS request: %s\n", err)
+		os.Exit(1)
+	}
+
+	joinCommand := fmt.Sprintf(`["4","4","lv:%s","phx_join",{"url":"%s","params":{"_csrf_token":"%s","timezone":"America/New_York","_mounts":0},"session":"%s","static":"%s"}]`, viewId, pageUrl, csrfToken, viewSession, viewStatic)
+	if _, err := ws.Write([]byte(joinCommand)); err != nil {
+		fmt.Printf("error sending WS command: %s\n", err)
+		os.Exit(1)
+	}
+
+	var msg = make([]byte, 512)
+	var n int
+	if n, err = ws.Read(msg); err != nil {
+		fmt.Printf("error received WS response: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Received: %s.\n", msg[:n])
 }
 
 func parseCsrfToken(text string) string {
@@ -45,14 +71,33 @@ func parseCsrfToken(text string) string {
 			return ""
 		case tokenType == html.StartTagToken:
 			token := tokenizer.Token()
-			if token.Data == "meta" && getAttribute("name", token) == "csrf-token" {
-				return getAttribute("content", token)
+			if token.Data == "meta" && getAttribute(token, "name") == "csrf-token" {
+				return getAttribute(token, "content")
 			}
 		}
 	}
 }
 
-func getAttribute(name string, token html.Token) string {
+func parseLiveViewData(text string) (string, string, string) {
+	tokenizer := html.NewTokenizer(strings.NewReader(text))
+
+	for {
+		tokenType := tokenizer.Next()
+		switch {
+		case tokenType == html.ErrorToken:
+			return "", "", ""
+		case tokenType == html.StartTagToken:
+			token := tokenizer.Token()
+			if token.Data == "div" && getAttribute(token, "data-phx-main") == "true" {
+				return getAttribute(token, "id"),
+					getAttribute(token, "data-phx-session"),
+					getAttribute(token, "data-phx-static")
+			}
+		}
+	}
+}
+
+func getAttribute( token html.Token, name string) string {
 	for _, attribute := range token.Attr {
 		if attribute.Key == name {
 			return attribute.Val
